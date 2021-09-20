746c10f60a27dad3eaaef4e4d9886e8cb75afd3a
evaldas1994 <evaldas.tuleikis@gmail.com>
2021-09-20 13:38:55 +0300

    load from controller

---
+++ app/template/main/post/index.php
@@ -0,0 +1,1 @@
+<h1>cia post index failas</h1>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 include_once '../vendor/autoload.php';
+include_once '../config.php';
+
+use Controller\PostController;
+
+$postController = new PostController();
+$postController->index();
